d21c2da317fbb4af8814e75c22fe1f46983d89a4
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2019-12-10 09:20:58 +0100

    initial commit

---
+++ NumberGenerator.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using NumberGenerator.Logic;
+
+namespace NumberGenerator.ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyNumberGenerator generator = new MyNumberGenerator(100);
+            generator.Attach(new TextView());
+            generator.Attach(new CounterView());
+            generator.Attach(new BarView());
+            generator.Attach(new SumView());
+
+            generator.Start();
+        }
+    }
+}
---
+++ NumberGenerator.Logic/MyNumberGenerator.cs
@@ -0,0 +1,73 @@
+﻿using NumberGenerator.Logic.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NumberGenerator.Logic
+{
+    public class MyNumberGenerator : IObservable
+    {
+        private const int RANDOM_MIN_VALUE = 1;
+        private const int RANDOM_MAX_VALUE = 10;
+        private const int MSECONDS_TO_SLEEP = 750;
+
+        private readonly int _nrOfGenerations;
+        private readonly IList<IObserver> _observers; 
+
+        public MyNumberGenerator(int nrOfGenerations)
+        {
+            _nrOfGenerations = nrOfGenerations;
+            _observers = new List<IObserver>();
+        }
+
+        public void Attach(IObserver observer)
+        {
+            if (observer == null)
+            {
+                throw new ArgumentNullException(nameof(observer));
+            }
+
+            if (_observers.Contains(observer))
+            {
+                throw new InvalidOperationException("Observer is already attached!");
+            }
+
+            _observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            if (observer == null)
+            {
+                throw new ArgumentNullException(nameof(observer));
+            }
+
+            if (!_observers.Contains(observer))
+            {
+                throw new InvalidOperationException("Observer was not attached!");
+            }
+
+            _observers.Remove(observer);
+        }
+
+        public void Start()
+        {
+            Random random = new Random();
+            for (int i = 0; i < _nrOfGenerations; i++)
+            {
+                int newNumber = random.Next(RANDOM_MIN_VALUE, RANDOM_MAX_VALUE + 1);
+                OnNewNumber(newNumber);
+                Task.Delay(MSECONDS_TO_SLEEP).Wait();
+                Console.WriteLine("-----------------------------");
+            }
+        }
+
+        private void OnNewNumber(int newNumber)
+        {
+            foreach (IObserver observer in _observers)
+            {
+                observer.Update(newNumber);
+            }
+        }
+    }
+}
