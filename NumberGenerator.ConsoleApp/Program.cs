c134e88f003558a902c30dfb8447f413473bf141
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2019-12-12 18:13:05 +0100

    remove comments

--- NumberGenerator.ConsoleApp/Program.cs
+++ NumberGenerator.ConsoleApp/Program.cs
@@ -7,10 +7,6 @@ namespace NumberGenerator.ConsoleApp
         static void Main()
         {
             MyNumberGenerator generator = new MyNumberGenerator(100);
-            //generator.Attach(new TextView());
-            //generator.Attach(new CounterView());
-            //generator.Attach(new BarView());
-            //generator.Attach(new SumView());
 
             TextView textView = new TextView();
             BarView barView = new BarView();
--- NumberGenerator.Logic/MyNumberGenerator.cs
+++ NumberGenerator.Logic/MyNumberGenerator.cs
@@ -3,42 +3,20 @@ using System.Threading.Tasks;
 
 namespace NumberGenerator.Logic
 {
-    public class MyNumberGenerator 
+    public class MyNumberGenerator
     {
         private const int RANDOM_MIN_VALUE = 1;
         private const int RANDOM_MAX_VALUE = 10;
         private const int MSECONDS_TO_SLEEP = 750;
 
         private readonly int _nrOfGenerations;
-        //private readonly IList<IObserver> _observers; 
 
         public delegate void NumberChangedHandler(int newNumber);
 
         public MyNumberGenerator(int nrOfGenerations)
         {
             _nrOfGenerations = nrOfGenerations;
-            //_observers = new List<IObserver>();
         }
-
-        //public void Attach(IObserver observer)
-        //{
-        //    if (observer == null)
-        //    {
-        //        throw new ArgumentNullException(nameof(observer));
-        //    }
-
-        //    _observers.Add(observer);
-        //}
-
-        //public void Detach(IObserver observer)
-        //{
-        //    if (observer == null)
-        //    {
-        //        throw new ArgumentNullException(nameof(observer));
-        //    }
-
-        //    _observers.Remove(observer);
-        //}
 
         public void Start()
         {
@@ -54,13 +32,9 @@ namespace NumberGenerator.Logic
 
         private void OnNewNumber(int newNumber)
         {
-            //foreach (IObserver observer in _observers)
-            //{
-            //    observer.Update(newNumber);
-            //}
-                NumberChanged?.Invoke(newNumber);
+            NumberChanged?.Invoke(newNumber);
         }
 
-        public NumberChangedHandler NumberChanged { get; set ; }
+        public NumberChangedHandler NumberChanged { get; set; }
     }
 }
